9610d1ff353e469eff5fbabb5ec0deeb12871891
Anatoliy Stepanchenko <stepanchenko1991@gmail.com>
2020-10-07 10:52:47 +0300

    add bootstrap and fontawesome

--- app/javascript/components/TodoApp.js
+++ app/javascript/components/TodoApp.js
@@ -1,10 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faCameraRetro } from '@fortawesome/fontawesome-free-solid'
+
 class TodoApp extends React.Component {
   render () {
     return (
       <React.Fragment>
-          <h1>Hello</h1>
+        <a href="#" className="btn btn-primary my-2">
+            <FontAwesomeIcon icon="camera-retro" />Take a photo
+        </a>
       </React.Fragment>
     );
   }
