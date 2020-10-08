ffcb2f9105dc5a5f041b63e65dd3bf8b6f92a085
Anatoliy Stepanchenko <stepanchenko1991@gmail.com>
2020-10-08 19:19:53 +0300

    fix logout

--- app/javascript/components/TodoApp.js
+++ app/javascript/components/TodoApp.js
@@ -2,7 +2,8 @@ import React from "react"
 import TodoLists from "./todo_list/TodoLists";
 
 class TodoApp extends React.Component {
-    handleLogOut() {
+    handleLogOut(e) {
+      e.preventDefault();
       const csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
       fetch('/users/sign_out.json', {
           method: 'DELETE',
@@ -10,40 +11,14 @@ class TodoApp extends React.Component {
               'Content-Type': 'application/json',
               'X-CSRF-Token': csrf
           },
+      }).then(res => {
+          document.location.reload();
       })
     }
 
     render() {
         return (
             <React.Fragment>
-                {/*<a href="#" className="btn btn-primary my-2" onClick={e => e.preventDefault()}>*/}
-                {/*    <FontAwesomeIcon icon="camera-retro" />Take a photo*/}
-                {/*</a>*/}
-                {/*  <div className="container">*/}
-                {/*      <div className="row">*/}
-                {/*          <div className="col">*/}
-
-                {/*          </div>*/}
-                {/*          <div className="col-6">*/}
-
-                {/*          </div>*/}
-                {/*          <div className="col">*/}
-                {/*              <User user_email={this.props.user_email}/>*/}
-                {/*          </div>*/}
-                {/*      </div>*/}
-                {/*      <div className="row">*/}
-                {/*          <div className="col">*/}
-
-                {/*          </div>*/}
-                {/*          <div className="col-5">*/}
-                {/*              <TodoLists/>*/}
-                {/*          </div>*/}
-                {/*          <div className="col">*/}
-
-                {/*          </div>*/}
-                {/*      </div>*/}
-                {/*  </div>*/}
-
                 <div className="container container-fluid text-center">
                     <div className="row">
                         <div className="col-xs-8 col-xs-offset-2 text-center">
@@ -52,7 +27,7 @@ class TodoApp extends React.Component {
                                     <div className="collapse navbar-collapse"
                                          id="myNavbar">
                                         <ul className="nav navbar-nav navbar-right">
-                                            <li><a href="/users/sign_in"
+                                            <li><a href="#"
                                                    onClick={this.handleLogOut}><span
                                                 className="glyphicon glyphicon-log-in"/> LogOut</a>
                                             </li>
